b1a978e27f0cc844a9576a406fd23271fba2cf6f
pablopy <pgimher@upv.edu.es>
2024-04-18 17:07:30 +0200

    Primera versión de este paquete.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This Python fragment starts by initializing a list called "peliculas" containing movie titles. Then, it does nothing with the list.
+After defining the list, there is a function named "instrucciones" defined. This function takes no input. It prints three smart sentences in the standard output about the essentials of Python modules and packages. It prints one sentence each second.
+Actually this is a dummy package, it has only test purpouses.
---
+++ src/modulecio.py
@@ -0,0 +1,18 @@
+#! /bin/env python3
+
+import time
+
+peliculas = ["La vida de Bryan", "Matrix", "Torrente"]
+
+
+def instrucciones():
+    """Print essencial information about Python modules and packages.
+    """
+    time.sleep(1)
+    print("Un módulo Python es un fichero que contiene código Python")
+    time.sleep(1)
+    print("Un paquete Python agrupa varios módulos y submódulos.")
+    time.sleep(1)
+    print("Para que un directorio se trasforme en paquete Python, agrégale un \
+__init.py__")
+    time.sleep(1)
