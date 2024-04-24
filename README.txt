66ef02f1bffa72faa635a843177df7f4d68a46e4
pablopy <pgimher@upv.edu.es>
2024-04-24 13:52:32 +0200

    Version 0.0.3

--- README.txt
+++ README.txt
@@ -2,3 +2,7 @@ This Python fragment starts by initializing a list called "peliculas" containing
 After defining the list, there is a function named "instrucciones" defined. This function takes no input. It prints three smart sentences in the standard output about the essentials of Python modules and packages. It prints one sentence each second.
 Actually this is a dummy package, it has only test purpouses.
 In the new 0.0.2 and 0.0.2.1 versions, there is an additional sentence that says: "¡Has difrutado de la segunda versión de este paquete!"
+In the new version 0.0.3, you can add tabs to the “instructions” output when you call it with the optional “tabs” parameter set to True. This is an example of how to add functionality in a safe way in a library (without messing up the old one).
+You can build this package while running:
+$ python -m build
+(in the root of the repository: https://github.com/p4bloOS/paquete-cib-pablopy)
--- src/modulecio.py
+++ src/modulecio.py
@@ -5,15 +5,20 @@ import time
 peliculas = ["La vida de Bryan", "Matrix", "Torrente"]
 
 
-def instrucciones():
+def instrucciones(tabulacion=False):
     """Print essencial information about Python modules and packages.
     """
-    time.sleep(1)
-    print("Un módulo Python es un fichero que contiene código Python")
-    time.sleep(1)
-    print("Un paquete Python agrupa varios módulos y submódulos.")
-    time.sleep(1)
-    print("Para que un directorio se trasforme en paquete Python, agrégale un \
-__init.py__")
-    time.sleep(1)
-    print("¡Has difrutado de la segunda versión de este paquete!")
+    lista_instrucciones = [
+        "Un módulo Python es un fichero que contiene código Python",
+        "Un paquete Python agrupa varios módulos y submódulos.",
+        "Para que un directorio se trasforme en paquete Python, agrégale un \
+__init.py__",
+        "¡Has difrutado de la tercera versión de este paquete!"
+    ]
+    espacios = ""
+
+    for instr in lista_instrucciones:
+        time.sleep(1)
+        print(espacios + instr)
+        if tabulacion:
+            espacios = espacios + "    "
